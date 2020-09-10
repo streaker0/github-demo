cf340ed2ffddd3aa1af94fed23f0de22c3089c8f
streaker0 <71048454+streaker0@users.noreply.github.com>
2020-09-09 20:27:54 -0400

    Update contributors.js
    add my name to list of contributors

--- assets/contributors.js
+++ assets/contributors.js
@@ -6,5 +6,9 @@ export const contributors = [
   {
     name: 'cuHacking Example',
     githubProfile: 'https://github.com/cuHacking'
+  },
+  {
+  name:'Ayo',
+  githubProfile:'https://github.com/streaker0'
   }
 ]
